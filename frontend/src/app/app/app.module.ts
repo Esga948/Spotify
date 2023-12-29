61c7b81302ea50c4742586fb9df2c4c4a3eb2c0a
esga948 <esga948@vidalibarraquer.net>
2023-12-29 12:52:46 +0100

    frontend y cambio de carpetas

---
+++ frontend/src/app/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TslibModule } from '../tslib/tslib.module';
+import { AppComponent } from '../app.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppModule { }
--- src/controller/tokenController.js
+++
@@ -1,8 +0,0 @@
-usuarioAppController.inicio = function (req, res) {
-    res.sendFile("C:/Users/estel.garces/Spotify/src/views/inicio.html");
-  };
-  
-  usuarioAppController.redirectInicio = function (req, res) {
-    // Redirige a la página de inicio con el userId como parámetro
-    res.redirect(`/inicio?userId=${req.session.userId}`);
-  };
